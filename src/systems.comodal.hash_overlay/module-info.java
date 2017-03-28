959a133382429ffe3092a5351c3bcb77fc1d43fb
jamespedwards42 <james.p.edwards42@gmail.com>
2017-03-28 09:22:40 -0400

    Release with module-info.

--- src/systems.comodal.hash_overlay/module-info.java
+++ src/systems.comodal.hash_overlay/module-info.java
@@ -2,4 +2,7 @@ module systems.comodal.hash_overlay {
   exports systems.comodal.hash;
   exports systems.comodal.hash.api;
   exports systems.comodal.hash.multihash;
+
+  exports systems.comodal.hash.addresses;
+  exports systems.comodal.hash.addresses.api;
 }
